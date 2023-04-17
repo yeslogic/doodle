6c6ae6aac30a8dab8cd55cbbcf115830a52ae058
brendanzab <bjzaba@yahoo.com.au>
2023-04-17 10:33:01 +1000

    Reorganise tests

--- tests/decode.rs
+++ tests/decode.rs
@@ -14,30 +14,42 @@ fn doodle() -> Command {
     Command::new(env!("CARGO_BIN_EXE_doodle"))
 }
 
-#[test]
-fn test_decode_test_png() {
-    let output = doodle().args(["test.png"]).output().unwrap();
-    let expected = expect_test::expect_file!("expected/decode/test.png.stdout");
-    expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+mod jpg {
+    use super::*;
+
+    #[test]
+    fn test_decode_test_jpg() {
+        let output = doodle().args(["test.jpg"]).output().unwrap();
+        let expected = expect_test::expect_file!("expected/decode/test.jpg.stdout");
+        expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+    }
+
+    #[test]
+    fn test_decode_test2_jpg() {
+        let output = doodle().args(["test2.jpg"]).output().unwrap();
+        let expected = expect_test::expect_file!("expected/decode/test2.jpg.stdout");
+        expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+    }
 }
 
-#[test]
-fn test_decode_test_jpg() {
-    let output = doodle().args(["test.jpg"]).output().unwrap();
-    let expected = expect_test::expect_file!("expected/decode/test.jpg.stdout");
-    expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+mod png {
+    use super::*;
+
+    #[test]
+    fn test_decode_test_png() {
+        let output = doodle().args(["test.png"]).output().unwrap();
+        let expected = expect_test::expect_file!("expected/decode/test.png.stdout");
+        expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+    }
 }
 
-#[test]
-fn test_decode_test2_jpg() {
-    let output = doodle().args(["test2.jpg"]).output().unwrap();
-    let expected = expect_test::expect_file!("expected/decode/test2.jpg.stdout");
-    expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
-}
+mod riff {
+    use super::*;
 
-#[test]
-fn test_decode_test_webp() {
-    let output = doodle().args(["test.webp"]).output().unwrap();
-    let expected = expect_test::expect_file!("expected/decode/test.webp.stdout");
-    expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+    #[test]
+    fn test_decode_test_webp() {
+        let output = doodle().args(["test.webp"]).output().unwrap();
+        let expected = expect_test::expect_file!("expected/decode/test.webp.stdout");
+        expected.assert_eq(&String::from_utf8_lossy(&output.stdout));
+    }
 }
