ec564520ec21c94bbd413965e05878de83ca9755
brendanzab <bjzaba@yahoo.com.au>
2023-05-09 13:51:19 +1000

    Use consistent indentation
    The indentation was a bit inconsistent. I decided to go with two spaces as this
    tends to be pretty common in front-end code these days.

--- frontend/display.js
+++ frontend/display.js
@@ -24,83 +24,83 @@ function jsonToHTML(json) {
 }
 
 function seqToHTML(seq) {
-    if (isRecordSeq(seq)) {
-        let fields = [];
-        for (let [name, value] of seq[0]["Record"]) {
-            fields.push([name, getAtomicType(value)]);
-        }
-        return renderSeqTable(seq, fields);
-    } else {
-        let ul = document.createElement('ul');
-        for (item of seq) {
-            let li = document.createElement('li');
-            ul.appendChild(li);
-            li.classList.add(typeof item);
-            const content = jsonToHTML(item);
-            li.appendChild(content);
-        }
-        return ul;
+  if (isRecordSeq(seq)) {
+    let fields = [];
+    for (let [name, value] of seq[0]["Record"]) {
+      fields.push([name, getAtomicType(value)]);
     }
+    return renderSeqTable(seq, fields);
+  } else {
+    let ul = document.createElement('ul');
+    for (item of seq) {
+      let li = document.createElement('li');
+      ul.appendChild(li);
+      li.classList.add(typeof item);
+      const content = jsonToHTML(item);
+      li.appendChild(content);
+    }
+    return ul;
+  }
 }
 
 function recordToHTML(fields) {
-    if (isFlatRecord(fields)) {
-        return renderRecordTable(fields);
-    } else {
-        let ul = document.createElement('ul');
-        for (field of fields) {
-            let li = document.createElement('li');
-            ul.appendChild(li);
-            li.classList.add(typeof field);
-            const content = fieldToHTML(field);
-            li.appendChild(content);
-        }
-        return ul;
+  if (isFlatRecord(fields)) {
+    return renderRecordTable(fields);
+  } else {
+    let ul = document.createElement('ul');
+    for (field of fields) {
+      let li = document.createElement('li');
+      ul.appendChild(li);
+      li.classList.add(typeof field);
+      const content = fieldToHTML(field);
+      li.appendChild(content);
     }
+    return ul;
+  }
 }
 
 function isRecordSeq(seq) {
-    return seq.length > 0 && (typeof seq[0] === "object") && ("Record" in seq[0]) && isFlatRecord(seq[0]["Record"]);
+  return seq.length > 0 && (typeof seq[0] === "object") && ("Record" in seq[0]) && isFlatRecord(seq[0]["Record"]);
 }
 
 function isFlatRecord(fields) {
-    for (let [name, value] of fields) {
-        if (!isAtomicValue(value) && getFieldASCII(name, value) === null) {
-            return false;
-        }
+  for (let [name, value] of fields) {
+    if (!isAtomicValue(value) && getFieldASCII(name, value) === null) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 function isAtomicValue(value) {
-    return getAtomicType(value) !== null;
+  return getAtomicType(value) !== null;
 }
 
 function getAtomicType(value) {
-    const atomicTypes = ["U8", "U16", "U32"];
-    for (const type of atomicTypes) {
-        if (type in value) return type;
-    }
-    return null;
+  const atomicTypes = ["U8", "U16", "U32"];
+  for (const type of atomicTypes) {
+    if (type in value) return type;
+  }
+  return null;
 }
 
 function getFieldASCII(name, value) {
-    if (name === "identifier" && ("Seq" in value)) {
-        // JPEG APP1 identifier
-        return value["Seq"];
-    } else if ((name === "signature" || name === "tag") && ("Tuple" in value)) {
-        // PNG signature and tags
-        return value["Tuple"];
-    } else if (name === "tag" && ("Variant" in value) && ("Tuple" in value["Variant"][1])) {
-        // more PNG tags
-        return value["Variant"][1]["Tuple"];
-    } else if (name === "version" && ("Seq" in value)) {
-        // GIF 89a version
-        return value["Seq"];
-    }
-    else {
-        return null;
-    }
+  if (name === "identifier" && ("Seq" in value)) {
+    // JPEG APP1 identifier
+    return value["Seq"];
+  } else if ((name === "signature" || name === "tag") && ("Tuple" in value)) {
+    // PNG signature and tags
+    return value["Tuple"];
+  } else if (name === "tag" && ("Variant" in value) && ("Tuple" in value["Variant"][1])) {
+    // more PNG tags
+    return value["Variant"][1]["Tuple"];
+  } else if (name === "version" && ("Seq" in value)) {
+    // GIF 89a version
+    return value["Seq"];
+  }
+  else {
+    return null;
+  }
 }
 
 // Turn a Javascript object into a definition list element.
@@ -118,14 +118,14 @@ function objToDL(obj) {
     result.appendChild(dt);
 
     if (key === "Record") {
-        const content = recordToHTML(obj[key]);
-        dd.appendChild(content);
+      const content = recordToHTML(obj[key]);
+      dd.appendChild(content);
     } else if (key === "Seq") {
-        const content = seqToHTML(obj[key]);
-        dd.appendChild(content);
+      const content = seqToHTML(obj[key]);
+      dd.appendChild(content);
     } else {
-        const content = jsonToHTML(obj[key]);
-        dd.appendChild(content);
+      const content = jsonToHTML(obj[key]);
+      dd.appendChild(content);
     }
 
     result.appendChild(dd);
@@ -134,108 +134,108 @@ function objToDL(obj) {
 }
 
 function fieldToHTML([name, value]) {
-    let ul = document.createElement('ul');
-    let liName = document.createElement('li');
-    let liValue = document.createElement('li');
-    liName.classList.add(typeof name);
-    liValue.classList.add(typeof value);
+  let ul = document.createElement('ul');
+  let liName = document.createElement('li');
+  let liValue = document.createElement('li');
+  liName.classList.add(typeof name);
+  liValue.classList.add(typeof value);
 
-    const nameContent = jsonToHTML(name);
-    liName.appendChild(nameContent);
+  const nameContent = jsonToHTML(name);
+  liName.appendChild(nameContent);
 
-    let valueContent;
-    let valueASCII = getFieldASCII(name, value);
-    if (valueASCII !== null) {
-        valueContent = renderASCII(valueASCII);
-    } else {
-        valueContent = jsonToHTML(value);
-    }
-    liValue.appendChild(valueContent);
+  let valueContent;
+  let valueASCII = getFieldASCII(name, value);
+  if (valueASCII !== null) {
+    valueContent = renderASCII(valueASCII);
+  } else {
+    valueContent = jsonToHTML(value);
+  }
+  liValue.appendChild(valueContent);
 
-    ul.appendChild(liName);
-    ul.appendChild(liValue);
-    return ul;
+  ul.appendChild(liName);
+  ul.appendChild(liValue);
+  return ul;
 }
 
 function renderRecordTable(record) {
-    let table = document.createElement("table");
-    table.border = 1;
-    for (let [name, value] of record) {
-        let tr = document.createElement("tr");
-        table.appendChild(tr);
-        let th = document.createElement("th");
-        tr.appendChild(th);
-        th.textContent = name;
-        let td = document.createElement("td");
-        tr.appendChild(td);
-        let content;
-        let valueASCII = getFieldASCII(name, value);
-        if (valueASCII !== null) {
-            content = renderASCII(valueASCII);
-        } else {
-            content = jsonToHTML(value);
-        }
-        td.appendChild(content);
+  let table = document.createElement("table");
+  table.border = 1;
+  for (let [name, value] of record) {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = name;
+    let td = document.createElement("td");
+    tr.appendChild(td);
+    let content;
+    let valueASCII = getFieldASCII(name, value);
+    if (valueASCII !== null) {
+      content = renderASCII(valueASCII);
+    } else {
+      content = jsonToHTML(value);
     }
-    return table;
+    td.appendChild(content);
+  }
+  return table;
 }
 
 function renderSeqTable(seq, fields) {
-    let table = document.createElement("table");
-    table.border = 1;
+  let table = document.createElement("table");
+  table.border = 1;
+  let tr = document.createElement("tr");
+  table.appendChild(tr);
+  let map = {};
+  for (let [name, type] of fields) {
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = name + " : " + type;
+    map[name] = type;
+  }
+  for (let item of seq) {
+    let record = item["Record"];
     let tr = document.createElement("tr");
     table.appendChild(tr);
-    let map = {};
-    for (let [name, type] of fields) {
-        let th = document.createElement("th");
-        tr.appendChild(th);
-        th.textContent = name + " : " + type;
-        map[name] = type;
+    for (let [name, value] of record) {
+      let type = map[name];
+      let td = document.createElement("td");
+      tr.appendChild(td);
+      let content = jsonToHTML(value[type]);
+      td.appendChild(content);
     }
-    for (let item of seq) {
-        let record = item["Record"];
-        let tr = document.createElement("tr");
-        table.appendChild(tr);
-        for (let [name, value] of record) {
-            let type = map[name];
-            let td = document.createElement("td");
-            tr.appendChild(td);
-            let content = jsonToHTML(value[type]);
-            td.appendChild(content);
-        }
-    }
-    return table;
+  }
+  return table;
 }
 
 function renderASCII(seq) {
-    const escapes = {
-        0x00: '0',
-        0x09: 't',
-        0x0A: 'n',
-        0x0D: 'r',
-    };
-    let span = document.createElement("span");
-    span.className = "text";
-    let run = null;
-    for (let item of seq) {
-        let b = item["U8"];
-        let type, text;
-        if (b >= 0x20 && b < 0x7F) {
-            type = "printable";
-            text = String.fromCharCode(b);
-        } else if (b in escapes) {
-            type = "escape";
-            text = "\\" + escapes[b];
-        } else {
-            type = "control";
-            text = "\\x" + b.toString(16).padStart(2, '0');
-        }
-        if (!run || run.className !== type) {
-            run = document.createElement("span");
-            run.className = type;
-            span.appendChild(run);
-        }
-        run.textContent += text;
+  const escapes = {
+    0x00: '0',
+    0x09: 't',
+    0x0A: 'n',
+    0x0D: 'r',
+  };
+  let span = document.createElement("span");
+  span.className = "text";
+  let run = null;
+  for (let item of seq) {
+    let b = item["U8"];
+    let type, text;
+    if (b >= 0x20 && b < 0x7F) {
+      type = "printable";
+      text = String.fromCharCode(b);
+    } else if (b in escapes) {
+      type = "escape";
+      text = "\\" + escapes[b];
+    } else {
+      type = "control";
+      text = "\\x" + b.toString(16).padStart(2, '0');
     }
-    return span;
+    if (!run || run.className !== type) {
+      run = document.createElement("span");
+      run.className = type;
+      span.appendChild(run);
+    }
+    run.textContent += text;
+  }
+  return span;
 }
