97d52602e01d175bef1bb2ab5f5bdbbfb632be4b
Peter Duchovni <caufeminecraft+github@gmail.com>
2023-12-01 14:34:10 +1100

    Label/string fixes, ParseCtxt, micro-test

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,24 @@
 pub use crate::byte_set::ByteSet;
 pub use crate::decoder::Scope;
 pub use crate::read::ReadCtxt;
+
+// FIXME - this model does not support split_at or backtracking to previous ReadCtxt states
+pub struct ParseCtxt<'a> {
+    input: ReadCtxt<'a>,
+}
+
+impl<'a> ParseCtxt<'a> {
+    pub fn new(bytes: &'a [u8]) -> ParseCtxt<'a> {
+        Self { input: ReadCtxt::new(bytes) }
+    }
+
+    pub const fn offset(&self) -> usize {
+        self.input.offset
+    }
+
+    pub fn read_byte(&mut self) -> Option<u8> {
+        let (ret, new_input) =  self.input.read_byte()?;
+        self.input = new_input;
+        Some(ret)
+    }
+}
