5fc0ce7cba253cc5bff64f7deb439c5b9bab130d
Michael Day <mikeday@yeslogic.com>
2024-02-06 23:22:03 +1100

    Oops, need to subtract 16 from mpeg4 atom size if it's 64-bit.

--- src/bin/doodle/format/mpeg4.rs
+++ src/bin/doodle/format/mpeg4.rs
@@ -22,6 +22,22 @@ pub fn main(module: &mut FormatModule, base: &BaseModule) -> FormatRef {
         ]),
     );
 
+    fn make_atom(base: &BaseModule, tag: FormatRef, data: Format) -> Format {
+        record([
+            ("size-field", base.u32be()),
+            ("type", tag.call()),
+            (
+                "size",
+                if_then_else(
+                    expr_eq(var("size-field"), Expr::U32(1)),
+                    map(base.u64be(), lambda("x", sub(var("x"), Expr::U64(16)))),
+                    Format::Compute(as_u64(sub(var("size-field"), Expr::U32(8)))),
+                ),
+            ),
+            ("data", Format::Slice(var("size"), Box::new(data))),
+        ])
+    }
+
     let ftyp_data = record([
         ("major_brand", base.u32be()),
         ("minor_version", base.u32be()),
@@ -30,288 +46,176 @@ pub fn main(module: &mut FormatModule, base: &BaseModule) -> FormatRef {
 
     let edts_atom = module.define_format(
         "mpeg4.edts-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![(
+                    Pattern::Wildcard,
+                    "unknown",
+                    Format::Repeat(Box::new(base.u8())),
+                )],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![(
-                            Pattern::Wildcard,
-                            "unknown",
-                            Format::Repeat(Box::new(base.u8())),
-                        )],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let dinf_atom = module.define_format(
         "mpeg4.dinf-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![(
+                    Pattern::Wildcard,
+                    "unknown",
+                    Format::Repeat(Box::new(base.u8())),
+                )],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![(
-                            Pattern::Wildcard,
-                            "unknown",
-                            Format::Repeat(Box::new(base.u8())),
-                        )],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let stbl_atom = module.define_format(
         "mpeg4.stbl-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![(
+                    Pattern::Wildcard,
+                    "unknown",
+                    Format::Repeat(Box::new(base.u8())),
+                )],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![(
-                            Pattern::Wildcard,
-                            "unknown",
-                            Format::Repeat(Box::new(base.u8())),
-                        )],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let minf_atom = module.define_format(
         "mpeg4.minf-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![
+                    (
+                        tag_pattern(['d', 'i', 'n', 'f']),
+                        "dinf",
+                        Format::Repeat(Box::new(dinf_atom.call())),
+                    ),
+                    (
+                        tag_pattern(['s', 't', 'b', 'l']),
+                        "stbl",
+                        Format::Repeat(Box::new(stbl_atom.call())),
+                    ),
+                    (
+                        Pattern::Wildcard,
+                        "unknown",
+                        Format::Repeat(Box::new(base.u8())),
+                    ),
+                ],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![
-                            (
-                                tag_pattern(['d', 'i', 'n', 'f']),
-                                "dinf",
-                                Format::Repeat(Box::new(dinf_atom.call())),
-                            ),
-                            (
-                                tag_pattern(['s', 't', 'b', 'l']),
-                                "stbl",
-                                Format::Repeat(Box::new(stbl_atom.call())),
-                            ),
-                            (
-                                Pattern::Wildcard,
-                                "unknown",
-                                Format::Repeat(Box::new(base.u8())),
-                            ),
-                        ],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let mdia_atom = module.define_format(
         "mpeg4.mdia-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![
+                    (
+                        tag_pattern(['m', 'i', 'n', 'f']),
+                        "minf",
+                        Format::Repeat(Box::new(minf_atom.call())),
+                    ),
+                    (
+                        Pattern::Wildcard,
+                        "unknown",
+                        Format::Repeat(Box::new(base.u8())),
+                    ),
+                ],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![
-                            (
-                                tag_pattern(['m', 'i', 'n', 'f']),
-                                "minf",
-                                Format::Repeat(Box::new(minf_atom.call())),
-                            ),
-                            (
-                                Pattern::Wildcard,
-                                "unknown",
-                                Format::Repeat(Box::new(base.u8())),
-                            ),
-                        ],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let trak_atom = module.define_format(
         "mpeg4.trak-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![
+                    (
+                        tag_pattern(['e', 'd', 't', 's']),
+                        "edts",
+                        Format::Repeat(Box::new(edts_atom.call())),
+                    ),
+                    (
+                        tag_pattern(['m', 'd', 'i', 'a']),
+                        "mdia",
+                        Format::Repeat(Box::new(mdia_atom.call())),
+                    ),
+                    (
+                        Pattern::Wildcard,
+                        "unknown",
+                        Format::Repeat(Box::new(base.u8())),
+                    ),
+                ],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![
-                            (
-                                tag_pattern(['e', 'd', 't', 's']),
-                                "edts",
-                                Format::Repeat(Box::new(edts_atom.call())),
-                            ),
-                            (
-                                tag_pattern(['m', 'd', 'i', 'a']),
-                                "mdia",
-                                Format::Repeat(Box::new(mdia_atom.call())),
-                            ),
-                            (
-                                Pattern::Wildcard,
-                                "unknown",
-                                Format::Repeat(Box::new(base.u8())),
-                            ),
-                        ],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let moov_atom = module.define_format(
         "mpeg4.moov-atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![
+                    (
+                        tag_pattern(['t', 'r', 'a', 'k']),
+                        "trak",
+                        Format::Repeat(Box::new(trak_atom.call())),
+                    ),
+                    (
+                        Pattern::Wildcard,
+                        "unknown",
+                        Format::Repeat(Box::new(base.u8())),
+                    ),
+                ],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![
-                            (
-                                tag_pattern(['t', 'r', 'a', 'k']),
-                                "trak",
-                                Format::Repeat(Box::new(trak_atom.call())),
-                            ),
-                            (
-                                Pattern::Wildcard,
-                                "unknown",
-                                Format::Repeat(Box::new(base.u8())),
-                            ),
-                        ],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     let atom = module.define_format(
         "mpeg4.atom",
-        record([
-            ("size-field", base.u32be()),
-            ("type", tag.call()),
-            (
-                "size",
-                if_then_else(
-                    expr_eq(var("size-field"), Expr::U32(1)),
-                    base.u64be(),
-                    Format::Compute(as_u64(var("size-field"))),
-                ),
+        make_atom(
+            base,
+            tag,
+            match_variant(
+                var("type"),
+                vec![
+                    (tag_pattern(['f', 't', 'y', 'p']), "ftyp", ftyp_data),
+                    (
+                        tag_pattern(['m', 'o', 'o', 'v']),
+                        "moov",
+                        Format::Repeat(Box::new(moov_atom.call())),
+                    ),
+                    (
+                        Pattern::Wildcard,
+                        "unknown",
+                        Format::Repeat(Box::new(base.u8())),
+                    ),
+                ],
             ),
-            (
-                "data",
-                Format::Slice(
-                    sub(var("size"), Expr::U64(8)),
-                    Box::new(match_variant(
-                        var("type"),
-                        vec![
-                            (tag_pattern(['f', 't', 'y', 'p']), "ftyp", ftyp_data),
-                            (
-                                tag_pattern(['m', 'o', 'o', 'v']),
-                                "moov",
-                                Format::Repeat(Box::new(moov_atom.call())),
-                            ),
-                            (
-                                Pattern::Wildcard,
-                                "unknown",
-                                Format::Repeat(Box::new(base.u8())),
-                            ),
-                        ],
-                    )),
-                ),
-            ),
-        ]),
+        ),
     );
 
     module.define_format(
