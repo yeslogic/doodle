a51d0e8657f14cc04cf7ce6e714f709684423b54
Peter Duchovni <caufeminecraft+github@gmail.com>
2024-03-20 12:55:16 +1100

    Add From<TypedXXX> for Format, Expr, Pattern

--- src/codegen/typed_format.rs
+++ src/codegen/typed_format.rs
@@ -79,10 +79,12 @@ impl TypedFormat<GenType> {
     pub(crate) fn expect_rust_type(&self, expected: &RustType) {
         match self {
             TypedFormat::FormatCall(_, _, _, gt_f) => gt_f.expect_rust_type(expected),
-            TypedFormat::Fail => unreachable!("TypedFormat::Fail has no equivalent RustType, expected {expected:?}"),
-            | TypedFormat::Align(_)
-            | TypedFormat::EndOfInput => assert!(matches!(expected, RustType::Atom(AtomType::Prim(PrimType::Unit)))),
-            TypedFormat::Byte(_) => assert!(matches!(expected, RustType::Atom(AtomType::Prim(PrimType::U8)))),
+            TypedFormat::Fail =>
+                unreachable!("TypedFormat::Fail has no equivalent RustType, expected {expected:?}"),
+            TypedFormat::Align(_) | TypedFormat::EndOfInput =>
+                assert!(matches!(expected, RustType::Atom(AtomType::Prim(PrimType::Unit)))),
+            TypedFormat::Byte(_) =>
+                assert!(matches!(expected, RustType::Atom(AtomType::Prim(PrimType::U8)))),
             | TypedFormat::Tuple(gt, ..)
             | TypedFormat::Record(gt, ..)
             | TypedFormat::Repeat(gt, ..)
@@ -103,16 +105,20 @@ impl TypedFormat<GenType> {
             | TypedFormat::Apply(gt, ..)
             | TypedFormat::Union(gt, ..)
             | TypedFormat::UnionNondet(gt, ..)
-            | TypedFormat::Variant(gt, ..) => match gt {
-                GenType::Inline(actual) => assert_eq!(actual, expected),
-                GenType::Def((ix1, lb1), ..) => match expected {
-                    RustType::Atom(AtomType::TypeRef(LocalType::LocalDef(ix0, lb0))) => {
-                        assert_eq!((ix0, lb0), (ix1, lb1));
-                    }
-                    _ => unreachable!("actual type GenType::Def({ix1}, {lb1}) != expected type ({expected:?}) [{self:?}]")
+            | TypedFormat::Variant(gt, ..) =>
+                match gt {
+                    GenType::Inline(actual) => assert_eq!(actual, expected),
+                    GenType::Def((ix1, lb1), ..) =>
+                        match expected {
+                            RustType::Atom(AtomType::TypeRef(LocalType::LocalDef(ix0, lb0))) => {
+                                assert_eq!((ix0, lb0), (ix1, lb1));
+                            }
+                            _ =>
+                                unreachable!(
+                                    "actual type GenType::Def({ix1}, {lb1}) != expected type ({expected:?}) [{self:?}]"
+                                ),
+                        }
                 }
-            }
-
         }
     }
 
@@ -328,10 +334,15 @@ pub enum TypedPattern<TypeRep> {
 }
 
 mod __impls {
-    use super::GenType;
-    use crate::codegen::{
-        rust_ast::{AtomType, CompType, PrimType, RustType, RustTypeDef},
-        IxLabel,
+    use std::f32::consts::E;
+
+    use super::{GenType, TypedDynFormat, TypedExpr, TypedFormat, TypedPattern};
+    use crate::{
+        codegen::{
+            rust_ast::{AtomType, CompType, PrimType, RustType, RustTypeDef},
+            IxLabel,
+        },
+        DynFormat, Expr, Format, Pattern,
     };
 
     impl From<RustType> for GenType {
@@ -363,6 +374,155 @@ mod __impls {
     impl From<AtomType> for GenType {
         fn from(value: AtomType) -> Self {
             GenType::Inline(RustType::from(value))
+        }
+    }
+
+    fn revec<T, U: From<T>>(v: Vec<T>) -> Vec<U> {
+        v.into_iter().map(U::from).collect()
+    }
+
+    fn revec_pair<A, X: From<A>, B, Y: From<B>>(v: Vec<(A, B)>) -> Vec<(X, Y)> {
+        v.into_iter()
+            .map(|(a, b)| (X::from(a), Y::from(b)))
+            .collect()
+    }
+
+    fn rebox<T, U: From<T>>(b: Box<T>) -> Box<U> {
+        Box::new(U::from(*b))
+    }
+
+    impl<TypeRep> From<TypedExpr<TypeRep>> for Expr {
+        fn from(value: TypedExpr<TypeRep>) -> Self {
+            match value {
+                TypedExpr::Var(_, lbl) => Expr::Var(lbl),
+                TypedExpr::Bool(b) => Expr::Bool(b),
+                TypedExpr::U8(n) => Expr::U8(n),
+                TypedExpr::U16(n) => Expr::U16(n),
+                TypedExpr::U32(n) => Expr::U32(n),
+                TypedExpr::U64(n) => Expr::U64(n),
+                TypedExpr::Tuple(_, t_elts) => Expr::Tuple(revec(t_elts)),
+                TypedExpr::TupleProj(_, tup, ix) => Expr::TupleProj(rebox(tup), ix),
+                TypedExpr::Record(_, t_flds) => Expr::Record(revec_pair(t_flds)),
+                TypedExpr::RecordProj(_, rec, fld) => Expr::RecordProj(rebox(rec), fld),
+                TypedExpr::Variant(_, name, inner) => Expr::Variant(name, rebox(inner)),
+                TypedExpr::Seq(_, t_elems) => Expr::Seq(revec(t_elems)),
+                TypedExpr::Match(_, head, branches) => {
+                    Expr::Match(rebox(head), revec_pair(branches))
+                }
+                TypedExpr::Lambda(_, name, inner) => Expr::Lambda(name, rebox(inner)),
+                TypedExpr::IntRel(_, rel, x, y) => Expr::IntRel(rel, rebox(x), rebox(y)),
+                TypedExpr::Arith(_, op, x, y) => Expr::Arith(op, rebox(x), rebox(y)),
+                TypedExpr::AsU8(x) => Expr::AsU8(rebox(x)),
+                TypedExpr::AsU16(x) => Expr::AsU16(rebox(x)),
+                TypedExpr::AsU32(x) => Expr::AsU32(rebox(x)),
+                TypedExpr::AsU64(x) => Expr::AsU64(rebox(x)),
+                TypedExpr::AsChar(x) => Expr::AsChar(rebox(x)),
+                TypedExpr::U16Be(x) => Expr::U16Be(rebox(x)),
+                TypedExpr::U16Le(x) => Expr::U16Le(rebox(x)),
+                TypedExpr::U32Be(x) => Expr::U32Be(rebox(x)),
+                TypedExpr::U32Le(x) => Expr::U32Le(rebox(x)),
+                TypedExpr::U64Be(x) => Expr::U64Be(rebox(x)),
+                TypedExpr::U64Le(x) => Expr::U64Le(rebox(x)),
+                TypedExpr::SeqLength(x) => Expr::SeqLength(rebox(x)),
+                TypedExpr::SubSeq(_, start, len, seq) => {
+                    Expr::SubSeq(rebox(start), rebox(len), rebox(seq))
+                }
+                TypedExpr::FlatMap(_, lambda, seq) => Expr::FlatMap(rebox(lambda), rebox(seq)),
+                TypedExpr::FlatMapAccum(_, lambda, acc, vt, seq) => {
+                    Expr::FlatMapAccum(rebox(lambda), rebox(acc), vt, rebox(seq))
+                }
+                TypedExpr::Dup(_, count, x) => Expr::Dup(rebox(count), rebox(x)),
+                TypedExpr::Inflate(_, x) => Expr::Inflate(rebox(x)),
+            }
+        }
+    }
+
+    impl<TypeRep> From<TypedFormat<TypeRep>> for Format {
+        fn from(value: TypedFormat<TypeRep>) -> Self {
+            match value {
+                TypedFormat::FormatCall(_gt, level, t_args, _) => {
+                    let args = t_args
+                        .into_iter()
+                        .map(|(_lbl, arg)| Expr::from(arg))
+                        .collect();
+                    Format::ItemVar(level, args)
+                }
+                TypedFormat::Fail => Format::Fail,
+                TypedFormat::EndOfInput => Format::EndOfInput,
+                TypedFormat::Align(n) => Format::Align(n),
+                TypedFormat::Byte(b) => Format::Byte(b),
+                TypedFormat::Variant(_, lbl, inner) => Format::Variant(lbl, rebox(inner)),
+                TypedFormat::Union(_, branches) => {
+                    Format::Union(branches.into_iter().map(Format::from).collect())
+                }
+                TypedFormat::UnionNondet(_, branches) => {
+                    Format::UnionNondet(branches.into_iter().map(Format::from).collect())
+                }
+                TypedFormat::Tuple(_, elts) => Format::Tuple(revec(elts)),
+                TypedFormat::Record(_, flds) => Format::Record(revec_pair(flds)),
+                TypedFormat::Repeat(_, inner) => Format::Repeat(rebox(inner)),
+                TypedFormat::Repeat1(_, inner) => Format::Repeat1(rebox(inner)),
+                TypedFormat::RepeatCount(_, count, inner) => {
+                    Format::RepeatCount(Expr::from(count), rebox(inner))
+                }
+                TypedFormat::RepeatUntilLast(_, lambda, inner) => {
+                    Format::RepeatUntilLast(Expr::from(lambda), rebox(inner))
+                }
+                TypedFormat::RepeatUntilSeq(_, lambda, inner) => {
+                    Format::RepeatUntilSeq(Expr::from(lambda), rebox(inner))
+                }
+                TypedFormat::Peek(_, inner) => Format::Peek(rebox(inner)),
+                TypedFormat::PeekNot(_, inner) => Format::PeekNot(rebox(inner)),
+                TypedFormat::Slice(_, sz, inner) => Format::Slice(Expr::from(sz), rebox(inner)),
+                TypedFormat::Bits(_, inner) => Format::Bits(rebox(inner)),
+                TypedFormat::WithRelativeOffset(_, ofs, inner) => {
+                    Format::WithRelativeOffset(ofs.into(), rebox(inner))
+                }
+                TypedFormat::Map(_, inner, lambda) => Format::Map(rebox(inner), Expr::from(lambda)),
+                TypedFormat::Compute(_, expr) => Format::Compute(Expr::from(expr)),
+                TypedFormat::Let(_, name, val, inner) => {
+                    Format::Let(name, Expr::from(val), rebox(inner))
+                }
+                TypedFormat::Match(_, head, t_branches) => {
+                    let branches = t_branches
+                        .into_iter()
+                        .map(|(p, f)| (Pattern::from(p), Format::from(f)))
+                        .collect();
+                    Format::Match(Expr::from(head), branches)
+                }
+                TypedFormat::Dynamic(_, name, dynf, inner) => {
+                    Format::Dynamic(name, DynFormat::from(dynf), rebox(inner))
+                }
+                TypedFormat::Apply(_, name, _) => Format::Apply(name),
+            }
+        }
+    }
+
+    impl<TypeRep> From<TypedDynFormat<TypeRep>> for DynFormat {
+        fn from(value: TypedDynFormat<TypeRep>) -> Self {
+            match value {
+                TypedDynFormat::Huffman(code_values, opt_code_lengths) => {
+                    DynFormat::Huffman(Expr::from(code_values), opt_code_lengths.map(Expr::from))
+                }
+            }
+        }
+    }
+
+    impl<TypeRep> From<TypedPattern<TypeRep>> for Pattern {
+        fn from(value: TypedPattern<TypeRep>) -> Self {
+            match value {
+                TypedPattern::Binding(_, name) => Pattern::Binding(name),
+                TypedPattern::Wildcard(_) => Pattern::Wildcard,
+                TypedPattern::Bool(b) => Pattern::Bool(b),
+                TypedPattern::U8(n) => Pattern::U8(n),
+                TypedPattern::U16(n) => Pattern::U16(n),
+                TypedPattern::U32(n) => Pattern::U32(n),
+                TypedPattern::U64(n) => Pattern::U64(n),
+                TypedPattern::Char(c) => Pattern::Char(c),
+                TypedPattern::Tuple(_, elts) => Pattern::Tuple(revec(elts)),
+                TypedPattern::Variant(_, name, inner) => Pattern::Variant(name, rebox(inner)),
+                TypedPattern::Seq(_, elts) => Pattern::Seq(revec(elts)),
+            }
         }
     }
 }
