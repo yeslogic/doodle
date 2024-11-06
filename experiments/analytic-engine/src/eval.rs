e028f0650f0fd77009cf33fc3896792bf6cfaafc
Peter Duchovni <caufeminecraft+github@gmail.com>
2024-11-06 19:58:01 +1100

    Refine eval model

--- experiments/analytic-engine/src/eval.rs
+++ experiments/analytic-engine/src/eval.rs
@@ -1,19 +1,29 @@
-use num_bigint::{BigInt, TryFromBigIntError};
-use num_traits::{
-    CheckedAdd, CheckedDiv, CheckedMul, CheckedRem, CheckedSub, SaturatingAdd, SaturatingMul,
-    SaturatingSub, WrappingAdd, WrappingMul, WrappingSub,
-};
+use num_bigint::BigInt;
+use num_traits::{CheckedAdd, CheckedDiv, CheckedMul, CheckedRem, CheckedSub};
 use std::any::type_name;
 use std::cell::LazyCell;
 use std::ops::{Add, Mul, Sub};
 use std::rc::Rc;
+
+#[derive(Clone)]
+pub struct IndirectEval {
+    value: Rc<LazyCell<BigInt, Box<dyn FnOnce() -> BigInt>>>,
+}
+
+impl std::fmt::Debug for IndirectEval {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("IndirectEval")
+            .field("value", &self.value)
+            .finish()
+    }
+}
 
 // SECTION - Evaluation model for potentially heterogenous machine integer operations
 #[derive(Clone, Debug)]
 pub enum Eval<T> {
     NaN,
     Direct(T),
-    Indirect(IndirectEval<T>),
+    Indirect(IndirectEval),
 }
 
 impl<T> Eval<T> {
@@ -40,28 +50,12 @@ where
     }
 }
 
-#[derive(Clone)]
-pub struct IndirectEval<T> {
-    value: Rc<LazyCell<BigInt, Box<dyn FnOnce() -> BigInt>>>,
-    saturated: T,
-    wrapped: T,
-}
-
-impl<T: std::fmt::Debug> std::fmt::Debug for IndirectEval<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("IndirectEval")
-            .field("value", &self.value)
-            .field("saturated", &self.saturated)
-            .field("wrapped", &self.wrapped)
-            .finish()
-    }
-}
 // !SECTION
 
 /// Macro for bulk definition of homogenously typed binary operations with a checked version provided by a trait (`num_traits`), where
 /// `None`` indicates underflow or overflow, falling back on available saturating and wrapping variants of the same operation for indirect computations
 macro_rules! homogenous {
-    ( $( $tr:ident, $meth:ident, $sat_tr:ident, $sat_meth:ident, $wrap_tr:ident, $wrap_meth:ident => $( ( $fname:ident , $t:ty ) ),+ $(,)? );+ $(;)? ) => {
+    ( $( $tr:ident, $meth:ident => $( ( $fname:ident , $t:ty ) ),+ $(,)? );+ $(;)? ) => {
         $(
             $(
                 pub fn $fname(lhs: $t, rhs: $t) -> Eval<$t> {
@@ -75,8 +69,6 @@ macro_rules! homogenous {
                                             BigInt::from(lhs).$meth(&BigInt::from(rhs)).unwrap()
                                         )
                                     )),
-                                    saturated: <$t as $sat_tr>::$sat_meth(&lhs, &rhs),
-                                    wrapped: <$t as $wrap_tr>::$wrap_meth(&lhs, &rhs),
                                 }
                             )
                         }
@@ -103,22 +95,7 @@ macro_rules! homogenous_quotient {
     }
 }
 
-/// Macro for bulk definition of homogenous source-type typed binary operations where the output type can represent every possible input type, but the computation
-macro_rules! widening {
-    ( $( $tr:ident, $meth:ident => $( ( $in_t:ty => $( ( $fname:ident, $out_t:ty ) ),+ $(,)? ) ),+ $(,)? );+ $(;)? ) => {
-        $(
-            $(
-                $(
-                    pub fn $fname(lhs: $in_t, rhs: $in_t) -> Eval<$out_t> {
-                        Eval::Direct(<$out_t as $tr>::$meth((lhs as $out_t), (rhs as $out_t)))
-                    }
-                )*
-            )*
-        )*
-    };
-}
-
-/// Macro for bulk definition of homogenous source-type typed binary operations where the output type can represent every possible input type, but the nominal
+/// Macro for bulk definition of homogenous source-type typed binary operations where the output type can represent every possible input type, but the computation might not succeed
 macro_rules! widening {
     ( $( $tr:ident, $meth:ident => $( ( $in_t:ty => $( ( $fname:ident, $out_t:ty ) ),+ $(,)? ) ),+ $(,)? );+ $(;)? ) => {
         $(
@@ -137,7 +114,7 @@ macro_rules! widening {
 
 // SECTION - strictly homogenous (T -> T -> T) binary operations
 homogenous! {
-    CheckedAdd, checked_add, SaturatingAdd, saturating_add, WrappingAdd, wrapping_add =>
+    CheckedAdd, checked_add =>
         (add_u8, u8),
         (add_u16, u16),
         (add_u32, u32),
@@ -146,7 +123,7 @@ homogenous! {
         (add_i16, i16),
         (add_i32, i32),
         (add_i64, i64);
-    CheckedSub, checked_sub, SaturatingSub, saturating_sub, WrappingSub, wrapping_sub =>
+    CheckedSub, checked_sub =>
         (sub_u8, u8),
         (sub_u16, u16),
         (sub_u32, u32),
@@ -155,7 +132,7 @@ homogenous! {
         (sub_i16, i16),
         (sub_i32, i32),
         (sub_i64, i64);
-    CheckedMul, checked_mul, SaturatingMul, saturating_mul, WrappingMul, wrapping_mul =>
+    CheckedMul, checked_mul =>
         (mul_u8, u8),
         (mul_u16, u16),
         (mul_u32, u32),
@@ -212,17 +189,15 @@ widening! {
 
 // !SECTION
 
-pub fn eval_fallback<Lhs, Rhs, Res, E>(
+pub fn eval_fallback<Lhs, Rhs, Res>(
     lhs: Lhs,
     rhs: Rhs,
     _op_hint: &'static str,
     checked_op: impl FnOnce(&BigInt, &BigInt) -> Option<BigInt>,
-    err_none: impl FnOnce() -> E,
-) -> Result<Res, E>
+) -> Eval<Res>
 where
     BigInt: From<Lhs> + From<Rhs>,
-    Res: TryFrom<BigInt, Error = TryFromBigIntError<BigInt>>,
-    E: From<TryFromBigIntError<BigInt>>,
+    Res: TryFrom<BigInt>,
 {
     eprintln!("[INFO]: encountered fallback operation `{_op_hint} : (({}, {}) -> {})` that may benefit from standalone function",
          type_name::<Lhs>(), type_name::<Rhs>(), type_name::<Res>()
@@ -231,38 +206,15 @@ where
     let big_r = BigInt::from(rhs);
     let o_big_res = checked_op(&big_l, &big_r);
     if let Some(big_res) = o_big_res {
-        Ok(<Res as TryFrom<BigInt>>::try_from(big_res)?)
+        let _big_res = big_res.clone();
+        if let Ok(res) = <Res as TryFrom<BigInt>>::try_from(big_res) {
+            Eval::Direct(res)
+        } else {
+            Eval::Indirect(IndirectEval {
+                value: Rc::new(LazyCell::new(Box::new(move || _big_res))),
+            })
+        }
     } else {
-        Err(err_none())
-    }
-}
-
-#[derive(Debug)]
-pub enum EvalError {
-    Downcast(TryFromBigIntError<BigInt>),
-    BadOperation,
-}
-
-impl std::fmt::Display for EvalError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            EvalError::Downcast(e) => write!(f, "failed to cast from BigInt: {e}"),
-            EvalError::BadOperation => write!(f, "bad operation (division or remainder by zero)"),
-        }
-    }
-}
-
-impl std::error::Error for EvalError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            EvalError::Downcast(e) => Some(e),
-            EvalError::BadOperation => None,
-        }
-    }
-}
-
-impl From<TryFromBigIntError<BigInt>> for EvalError {
-    fn from(value: TryFromBigIntError<BigInt>) -> EvalError {
-        Self::Downcast(value)
+        Eval::NaN
     }
 }
