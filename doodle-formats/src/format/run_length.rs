d58a159815aff7971e9256e2e8dddd78da75c209
Peter Duchovni <caufeminecraft+github@gmail.com>
2025-04-01 16:02:30 +1100

    Use common ascii-string-format to simplify rle

--- doodle-formats/src/format/run_length.rs
+++ doodle-formats/src/format/run_length.rs
@@ -1,5 +1,5 @@
 use crate::format::BaseModule;
-use doodle::{helper::*, Label, ValueType};
+use doodle::{helper::*, BaseType, Label, ValueType};
 use doodle::{FormatModule, FormatRef};
 
 pub fn main(module: &mut FormatModule, base: &BaseModule) -> FormatRef {
@@ -11,23 +11,25 @@ pub fn main(module: &mut FormatModule, base: &BaseModule) -> FormatRef {
             ("buf", repeat_count(var("len"), compute(var("char")))),
         ]),
     );
-    let run_type_os = module.get_format_type(run.get_level()).clone();
-    let rle_ascii_string_os = module.define_format_args(
-        "rle.old-style.ascii-string",
+    let ascii_string = module.define_format_args(
+        "base.func.ascii-string",
         vec![(
-            Label::Borrowed("runs"),
-            ValueType::Seq(Box::new(run_type_os)),
+            Label::Borrowed("input"),
+            ValueType::Seq(Box::new(ValueType::Base(BaseType::U8))),
         )],
-        compute(flat_map(
-            lambda("run", record_proj(var("run"), "buf")),
-            var("runs"),
-        )),
+        compute(var("input")),
     );
     let old_style = module.define_format(
         "rle.old-style",
         record([
             ("runs", repeat(run.call())),
-            ("data", rle_ascii_string_os.call_args(vec![var("runs")])),
+            (
+                "data",
+                ascii_string.call_args(vec![flat_map(
+                    lambda("run", record_proj(var("run"), "buf")),
+                    var("runs"),
+                )]),
+            ),
         ]),
     );
 
@@ -40,23 +42,17 @@ pub fn main(module: &mut FormatModule, base: &BaseModule) -> FormatRef {
                 ("buf", repeat_count(var("_len"), compute(var("_char")))),
             ]),
         );
-        let run_type_ns = module.get_format_type(run.get_level()).clone();
-        let rle_ascii_string_ns = module.define_format_args(
-            "rle.new-style.ascii-string",
-            vec![(
-                Label::Borrowed("runs"),
-                ValueType::Seq(Box::new(run_type_ns)),
-            )],
-            compute(flat_map(
-                lambda("run", record_proj(var("run"), "buf")),
-                var("runs"),
-            )),
-        );
         module.define_format(
             "rle.new-style",
             record_auto([
                 ("_runs", repeat(run.call())),
-                ("data", rle_ascii_string_ns.call_args(vec![var("_runs")])),
+                (
+                    "data",
+                    ascii_string.call_args(vec![flat_map(
+                        lambda("run", record_proj(var("run"), "buf")),
+                        var("_runs"),
+                    )]),
+                ),
             ]),
         )
     };
