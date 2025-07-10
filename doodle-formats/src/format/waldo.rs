a04adf0af4983b1363aa904f7a738b1fe07ff70e
Peter Duchovni <caufeminecraft+github@gmail.com>
2025-07-10 15:47:58 +1000

    Modify waldo format-ddeclaration to use view-formats

--- doodle-formats/src/format/waldo.rs
+++ doodle-formats/src/format/waldo.rs
@@ -1,5 +1,5 @@
-use doodle::helper::*;
-use doodle::{Format, FormatModule, FormatRef};
+use doodle::{helper::*, StyleHint};
+use doodle::{Expr, Format, FormatModule, FormatRef, Label, ViewFormat};
 
 use super::base::BaseModule;
 
@@ -13,12 +13,28 @@ pub fn main(module: &mut FormatModule, base: &BaseModule) -> FormatRef {
             ("_here", Format::Pos),
             (
                 "waldo",
-                Format::WithRelativeOffset(
-                    Box::new(var("_here")),
-                    Box::new(sub(var("where"), var("_here"))),
-                    Box::new(is_bytes(b"Waldo")),
+                Format::LetView(
+                    Label::Borrowed("scope"),
+                    Box::new(Format::Hint(
+                        StyleHint::AsciiStr,
+                        Box::new(Format::WithView(
+                            Label::Borrowed("scope"),
+                            ViewFormat::ReadOffsetLen(
+                                Box::new(sub(var("where"), var("_here"))),
+                                Box::new(Expr::U8(5)),
+                            ),
+                        )),
+                    )),
                 ),
             ),
+            // (
+            //     "waldo",
+            //     Format::WithRelativeOffset(
+            //         Box::new(var("_here")),
+            //         Box::new(sub(var("where"), var("_here"))),
+            //         Box::new(is_bytes(b"Waldo")),
+            //     ),
+            // ),
             ("__rem", Format::SkipRemainder),
         ]),
     )
