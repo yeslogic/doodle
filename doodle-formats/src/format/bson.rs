77585e3bfa1d66c7028db043bb51cf7cdafdd419
Peter Duchovni <caufeminecraft+github@gmail.com>
2026-09-11 16:38:25 +1000

    Local fix for permit(decodebytes()) bug

--- doodle-formats/src/format/bson.rs
+++ doodle-formats/src/format/bson.rs
@@ -1,12 +1,16 @@
-use doodle::{FormatModule, FormatRef, helper::*};
+use doodle::{Format, FormatModule, FormatRef, helper::*};
 
 fn cstring(module: &mut FormatModule, utf8_nz: FormatRef) -> FormatRef {
+    let utf8_nz_total = pseudo_record(
+        [("text", utf8_nz.call()), ("__eof", Format::EndOfInput)],
+        compute(var("text")),
+    );
     module.define_format(
         "bson.cstring",
         pseudo_record(
             [("bytes", repeat(not_byte(0x00))), ("__null", is_byte(0x00))],
             permit(
-                fmt_variant("valid", decode_bytes(var("bytes"), utf8_nz.call())),
+                fmt_variant("valid", decode_bytes(var("bytes"), utf8_nz_total)),
                 variant("invalid", var("bytes")),
             ),
         ),
